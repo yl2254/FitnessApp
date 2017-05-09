bea249882e79a0c7e96fb27686545015e555d22d
Yujia Liu <yl2254@cornell.com>
2017-05-08 21:17:57 -0700

    upload project

---
+++ FitnessBuddy/TaskRow.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import { StyleSheet, View, TouchableHighlight, Text} from 'react-native';
+/*
+const {
+    Text,
+    View,
+    import React, {Component} from 'react';
+    import { StyleSheet, View } from 'react-native';,
+} = React;
+*/
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: '#E7E7E7',
+        padding: 20,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+        marginLeft: 20,
+        marginRight: 20,
+    },
+    label: {
+        fontSize: 20,
+        fontWeight: '300',
+    },
+    doneButton: {
+        borderRadius: 5,
+        backgroundColor: '#EAEAEA',
+        padding: 5,
+    },
+});
+
+class TaskRow extends React.Component {
+    onDonePressed() {
+        this.props.onDone(this.props.todo);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text
+                    style={styles.label}
+                >{this.props.todo.task}</Text>
+
+                <TouchableHighlight
+                    onPress={this.onDonePressed.bind(this)}
+                    style={styles.doneButton}
+                >
+                    <Text>Done</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+}
+
+TaskRow.propTypes = {
+    onDone: React.PropTypes.func.isRequired,
+    todo: React.PropTypes.shape({
+        task: React.PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default TaskRow;
---
+++ FitnessBuddy/main.js
@@ -0,0 +1,98 @@
+
+/*const ReactNative = require('react-native');
+const React = require('react');
+
+const {
+    AppRegistry,
+    Component,
+    Navigator,
+} = React;
+*/
+
+import React, {Component} from 'react';
+import { StyleSheet, View, AppRegistry, Navigator, ListView} from 'react-native';
+
+import TaskList from './TaskList';
+import TaskForm from './TaskForm';
+
+class FitnessBuddy extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            todos: [
+                {
+                    task: 'Learn React Native',
+                },
+                {
+                    task: 'Learn Redux',
+                },
+            ],
+        };
+    }
+
+    onAddStarted() {
+        this.nav.push({
+            name: 'taskform',
+        });
+    }
+
+    onCancel() {
+        console.log('cancelled!');
+        this.nav.pop();
+    }
+
+    onAdd(task) {
+        console.log('a task was added: ', task);
+        this.state.todos.push({ task });
+        this.setState({ todos: this.state.todos });
+        this.nav.pop();
+    }
+
+    onDone(todo) {
+        console.log('todo was completed: ', todo.task);
+        const filteredTodos =
+            this.state.todos.filter((filterTodo) => {
+                return filterTodo !== todo;
+            });
+        this.setState({ todos: filteredTodos });
+    }
+
+    renderScene(route, nav) {
+        switch (route.name) {
+        case 'taskform':
+            return (
+                <TaskForm
+                    onAdd={this.onAdd.bind(this)}
+                    onCancel={this.onCancel.bind(this)}
+                />
+            );
+        default:
+            return (
+                <TaskList
+                    onAddStarted={this.onAddStarted.bind(this)}
+                    onDone={this.onDone.bind(this)}
+                    todos={this.state.todos}
+                />
+            );
+        }
+    }
+
+    configureScene() {
+        return Navigator.SceneConfigs.FloatFromBottom;
+    }
+
+    render() {
+        return (
+            <Navigator
+                configureScene={this.configureScene}
+                initialRoute={{ name: 'tasklist', index: 0 }}
+                ref={((nav) => {
+                    this.nav = nav;
+                })}
+                renderScene={this.renderScene.bind(this)}
+            />
+        );
+    }
+}
+
+AppRegistry.registerComponent('main', () => FitnessBuddy);
