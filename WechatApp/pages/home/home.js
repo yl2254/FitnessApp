7d6d1a5317d5d0939fdb985e93fc9eb2b545a00e
yl2254 <yl2254@cornell.edu>
2017-06-12 20:47:58 -0700

    commit

--- WechatApp/pages/home/home.js
+++ WechatApp/pages/home/home.js
@@ -1,18 +1,29 @@
 // home.js
+var app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    motto: 'Hello World',
+    userInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
+    })
   },
 
   /**
